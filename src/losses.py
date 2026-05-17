d157221d7019b2801b99435b15ebdb6223e4c367
PRABHA A <prabhaa.aids2023@citchennai.net>
2026-05-17 17:53:26 +0530

    Create losses.py

---
+++ src/losses.py
@@ -0,0 +1,24 @@
+python
+import numpy as np
+
+
+def cross_entropy_loss(y_true, y_pred):
+
+    epsilon = 1e-10
+
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+
+    loss = -np.mean(
+        np.sum(y_true * np.log(y_pred), axis=1)
+    )
+
+    return loss
+
+
+def squared_error_loss(y_true, y_pred):
+
+    loss = np.mean(
+        np.sum((y_true - y_pred) ** 2, axis=1)
+    )
+
+    return loss
